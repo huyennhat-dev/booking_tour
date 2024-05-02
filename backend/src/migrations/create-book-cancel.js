bf428ca730aae437f307b43d23263e1711f30b58
gechym <nguyenducbao166@gmail.com>
2024-05-02 10:36:32 +0700

    create cancel tour

---
+++ backend/src/migrations/create-book-cancel.js
@@ -0,0 +1,44 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // associate models
+    await queryInterface.createTable('Cancels', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      id_book: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      is_refund: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      stk: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      reason : {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: 'Tôi không muốn đi nữa'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cancels')
+  }
+}
